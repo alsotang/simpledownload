0189ecf139e04ae682aefba3cb373bc249eb14ba
alsotang <alsotang@gmail.com>
2015-07-29 15:09:46 +0800

    use aysnc method

--- index.js
+++ index.js
@@ -18,12 +18,7 @@ var download = function(url, filename, options, callback) {
       if (err) {
         return callback(err);
       }
-      try {
-        fs.writeFileSync(filename, res.text, {encoding: null});
-      } catch (e) {
-        return callback(e);
-      }
-      callback(null);
+      fs.writeFile(filename, res.text, {encoding: null}, callback);
     });
 };
 
